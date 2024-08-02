591f37f0017fc9b696054b5c5e30e070a2357c14
Brenden <153680020+brndnhrndz@users.noreply.github.com>
2024-08-02 16:28:54 -0400

    Hardcode decks/deck contents into source for debugging purposes

--- src/pages/DeckCollection/CollectionContainer.js
+++ src/pages/DeckCollection/CollectionContainer.js
@@ -1,14 +1,52 @@
 import React, { useState } from 'react'
 
 const CollectionContainer = () => {
-    const [decks, setDecks] = useState([]);
-
+    const [decks, setDecks] = useState([
+        // verbs
+        {
+            deckName: 'verbs',
+            cards: [
+                {
+                    index: 0,
+                    front: 'llegar',
+                    back: 'to arrive, to come'
+                },
+                {
+                    index: 1,
+                    front: 'subir',
+                    back: 'to go up'
+                },
+                {
+                    index: 2,
+                    front: 'poner',
+                    back: 'to put'
+                }
+            ]
+        },
+        // nouns
+        {
+            deckName: 'nouns',
+            cards: [
+                {
+                    index: 0,
+                    front: 'el espejo',
+                    back: 'the mirror'
+                },
+                {
+                    index: 1,
+                    front: 'el hombre',
+                    back: 'the man'
+                }
+            ]
+        }
+    ]);
+    
     return (
         <div id='collection-contaniner'>
             {
                 // replace with DeckItem component
                 decks.map((deck, index) => (
-                    <div key={index}>{deck}</div>
+                    <div key={index}>{deck.deckName}</div>
                 ))
             }
         </div>
