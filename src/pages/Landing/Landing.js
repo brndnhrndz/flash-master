faf38ea115b717566d4fbe4fe8215ebeb58e17c6
Brenden <153680020+brndnhrndz@users.noreply.github.com>
2024-08-02 13:26:45 -0400

    Add View Decks button and functionality to Landing component

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -6,12 +6,21 @@ const Landing = (props) => {
         props.setShowDeckCreation(true);
     }
 
+    const handleShowDecks = () => {
+        props.setShowLanding(false);
+        props.setShowDeckCollection(true);
+    }
+
     return (
         <div id='landing'>
             <h1>Flash Master</h1>
             <button type='button' name='create-deck-btn'
                 onClick={handleCreateDeck}>
                 Create Deck
+            </button>
+            <button type='button' name='view-decks-btn'
+                onClick={handleShowDecks}>
+                View Decks
             </button>
         </div>
     );
