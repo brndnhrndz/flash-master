63d3b9d4b9d1eada365d3e86f857133ffab405c8
Brenden <153680020+brndnhrndz@users.noreply.github.com>
2024-08-08 12:50:10 -0400

    Define <CardItem /> click behavior

--- src/pages/DeckContents/CardItem.js
+++ src/pages/DeckContents/CardItem.js
@@ -2,8 +2,19 @@ import React from 'react';
 import '../../assets/CardItem.css'
 
 const CardItem = ({ card, updateState }) => {
+    const handleCardItemClick = () => {
+        updateState({
+            activeCard: card,
+            activeCardFace: card.front,
+            showCardContents: false,
+            showCardView: true
+        });
+    };
+
     return (
-        <div className='card-item'>
+        <div
+            className='collection-item'
+            onClick={handleCardItemClick}>
             {card.front}
         </div>
     );
